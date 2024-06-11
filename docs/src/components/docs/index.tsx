9e0c1cbc39883f73af5361235992d7895e79f8cb
juji <jujiyangasli@gmail.com>
2024-06-12 05:00:23 +0700

    next14 and emotion still have fouc

--- docs/src/components/docs/index.tsx
+++ docs/src/components/docs/index.tsx
@@ -1,25 +1,29 @@
 'use client'
 
-import { css } from '@emotion/css'
+// import { css } from '@emotion/css'
 import DocsPage from './docs.mdx'
 import './style.css'
 
 export default function Docs(){
 
-  return <div className={css`
-    max-width: 800px;
-    padding: 0 16px;
-    margin: 0 auto;
-    line-height: 1.6rem;
-
-    @media screen and (min-width: 1207px){
-      width: 1207px;
-      display: grid;
-      grid-template-columns: 768px 190px;
-    }
-  `}>
+  return <div className="doc-container">
     <DocsPage />
   </div>
+
+  // return <div className={css`
+  //   max-width: 800px;
+  //   padding: 0 16px;
+  //   margin: 0 auto;
+  //   line-height: 1.6rem;
+
+  //   @media screen and (min-width: 1207px){
+  //     width: 1207px;
+  //     display: grid;
+  //     grid-template-columns: 768px 190px;
+  //   }
+  // `}>
+  //   <DocsPage />
+  // </div>
 
 }
 
